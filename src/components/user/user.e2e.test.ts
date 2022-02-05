e537ad7671614f23d7916f382df3799ad2421430
eduahcb <dev_eduardo@hotmail.com>
2022-02-05 18:59:36 -0300

    creating success spec

--- src/components/user/user.e2e.test.ts
+++ src/components/user/user.e2e.test.ts
@@ -4,31 +4,48 @@ import request from 'supertest'
 import App from '../../app'
 import { startConnection } from '../../config'
 
+import Factory from '../../helpers/factory'
+import startFactory from '../../utils/factories'
+
+import truncate from '../../helpers/truncate'
+
 describe('POST /users', () => {
   let app: express.Application
+
+  let factory: Factory
 
   beforeAll(async () => {
     await startConnection()
 
     app = new App().init()
+
+    factory = startFactory()
+  })
+
+  beforeEach(async () => {
+    await truncate()
+  })
+
+  afterAll(async () => {
+    await truncate()
   })
 
   test('should return a bad request', async () => {
-    const result = await request(app).post('/api/v1/users').send({
+    const userFactory = factory.build('user', {
       name: '',
-      email: '',
       password: '',
+      email: '',
     })
+
+    const result = await request(app).post('/api/v1/users').send(userFactory)
 
     expect(result.statusCode).toBe(400)
   })
 
   test('should create a new user', async () => {
-    const result = await request(app).post('/api/v1/users').send({
-      name: 'Eduardo',
-      email: 'test@test.com',
-      password: '23232',
-    })
+    const userFactory = factory.build('user')
+
+    const result = await request(app).post('/api/v1/users').send(userFactory)
 
     expect(result.statusCode).toBe(201)
   })
