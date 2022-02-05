a44c97944c820144dc6a1e2735814548c4631b77
eduahcb <dev_eduardo@hotmail.com>
2022-02-05 13:01:28 -0300

    add youch

--- src/middlewares/exceptionHandler.ts
+++ src/middlewares/exceptionHandler.ts
@@ -1,4 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
+import Youch from 'youch'
+
+import { config } from '../config'
 import errorHandler from '../handlers'
 
 export default async function exceptionHandler(
@@ -6,15 +9,26 @@ export default async function exceptionHandler(
   req: Request,
   res: Response,
   next: NextFunction
-): Promise<void> {
+): Promise<Response> {
   const isOperational = await errorHandler.handleError(error)
+
   if (!isOperational) {
-    res.status(500).json({
+    const env = config.get('env')
+
+    if (env === 'development') {
+      const youch = new Youch(error, req)
+      const result = await youch.toJSON()
+
+      return res.status(500).json(result)
+    }
+
+    return res.status(500).json({
       message: 'Internal server error',
       statusCode: 500,
     })
   }
-  res.status(error.statusCode).json({
+
+  return res.status(error.statusCode).json({
     message: error.message,
     statusCode: error.statusCode,
   })
