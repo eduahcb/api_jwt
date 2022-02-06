73abcdd9726d84734c0151b3ed776d5c8972c305
eduahcb <dev_eduardo@hotmail.com>
2022-02-06 02:31:23 -0300

    add verification if email already exists

--- src/components/user/user.form.test.ts
+++ src/components/user/user.form.test.ts
@@ -1,20 +1,52 @@
 import User from './user.entity'
 import UserForm from './user.form'
 
+jest.mock('typeorm', () => ({
+  ...jest.requireActual('typeorm'),
+  getRepository: jest.fn(() => ({
+    findOne: jest.fn(),
+  })),
+}))
+
+const { getRepository } = require('typeorm')
+
 describe('UserForm', () => {
-  test('should return a instance of User', () => {
+  test('should return a instance of User', async () => {
+    getRepository.mockImplementation(() => ({
+      findOne: jest.fn(() => undefined),
+    }))
+
     const userForm = new UserForm({
       name: 'Eduardo',
       email: 'test@test.com',
       password: '21212',
     })
 
-    const user = userForm.toModel()
+    const user = await userForm.toModel()
 
     expect(userForm).toBeInstanceOf(UserForm)
 
     expect(user.Name).toBe('Eduardo')
     expect(user.Email).toBe('test@test.com')
     expect(user).toBeInstanceOf(User)
+  })
+
+  test('should throw exception', async () => {
+    getRepository.mockImplementation(() => ({
+      findOne: jest.fn(async () => new User('test', 'email', 'e32332')),
+    }))
+
+    const userForm = new UserForm({
+      name: 'Eduardo',
+      email: 'test@test.com',
+      password: '21212',
+    })
+
+    try {
+      await userForm.toModel()
+    } catch (error: any) {
+      expect(error.message).toBe('email already exists')
+      expect(error.statusCode).toBe(400)
+    }
   })
 })
--- src/components/user/user.form.ts
+++ src/components/user/user.form.ts
@@ -1,9 +1,14 @@
+import { HttpStatusCode } from './../../helpers/HttpStatusCode'
+import { Repository, getRepository } from 'typeorm'
+import { HttpException } from '../../exceptions'
 import User from './user.entity'
 
 class UserForm {
   private name: string
   private email: string
   private password: string
+
+  private repository: Repository<User>
 
   constructor({
     name,
@@ -17,9 +22,19 @@ class UserForm {
     this.name = name
     this.email = email
     this.password = password
+    this.repository = getRepository(User)
   }
 
-  toModel(): User {
+  async toModel(): Promise<User> {
+    const user = await this.repository.findOne({ where: { email: this.email } })
+
+    if (user) {
+      throw new HttpException(
+        'email already exists',
+        HttpStatusCode.BAD_REQUEST
+      )
+    }
+
     return new User(this.name, this.email, this.password)
   }
 }
