c4f188e24f58f467bd928f0d37c0d5a09a674eb3
eduahcb <dev_eduardo@hotmail.com>
2022-02-10 18:29:55 -0300

    creating login route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express'
+
 import { userRouter } from './components/user'
+import { loginRouter } from './components/login'
 
 import { apiBaseUrl } from './config/constants/constants'
 
+import bearerAuth from './middlewares/bearerAuth'
+
 const routes = (): Router => {
   const routes = Router()
+
+  routes.use(`${apiBaseUrl}/login`, loginRouter())
+
+  routes.use(bearerAuth)
 
   routes.use(`${apiBaseUrl}/users`, userRouter())
 
