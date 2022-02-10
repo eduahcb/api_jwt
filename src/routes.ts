9f1ed76a3465202a5ac16d7364ce4151af3321df
eduahcb <dev_eduardo@hotmail.com>
2022-02-10 19:30:43 -0300

    removing bearerAuth

--- src/routes.ts
+++ src/routes.ts
@@ -5,14 +5,10 @@ import { loginRouter } from './components/login'
 
 import { apiBaseUrl } from './config/constants/constants'
 
-import bearerAuth from './middlewares/bearerAuth'
-
 const routes = (): Router => {
   const routes = Router()
 
   routes.use(`${apiBaseUrl}/login`, loginRouter())
-
-  routes.use(bearerAuth)
 
   routes.use(`${apiBaseUrl}/users`, userRouter())
 
