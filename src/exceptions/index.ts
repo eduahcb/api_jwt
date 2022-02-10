65078f96263315c329b2789f85bdffe6596349bb
eduahcb <dev_eduardo@hotmail.com>
2022-02-10 19:33:20 -0300

    exporting UnauthorizedException

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -1,2 +1,3 @@
 export { default as HttpException } from './HttpException'
 export { default as NotFoundException } from './NotFoundException'
+export { default as UnauthorizeException } from './UnauthorizedException'
