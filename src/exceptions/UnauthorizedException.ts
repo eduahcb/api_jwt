6bb3ad61d9d5e285311dd47658eb69a6bc6a206c
eduahcb <dev_eduardo@hotmail.com>
2022-02-10 19:32:52 -0300

    creating UnauthorizedException

---
+++ src/exceptions/UnauthorizedException.ts
@@ -0,0 +1,16 @@
+import { HttpStatusCode } from './../helpers/HttpStatusCode'
+
+class UnauthorizedException extends Error {
+  public statusCode: number
+  public isOperational: boolean
+
+  constructor() {
+    super()
+    this.message = 'Unauthorized'
+    this.statusCode = HttpStatusCode.UNAUTHORIZED
+    this.name = 'UnauthorizedException'
+    this.isOperational = true
+  }
+}
+
+export default UnauthorizedException
