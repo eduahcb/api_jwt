3c5af6b095a7938ab5a1f2caf5f9b2c7803288da
eduahcb <dev_eduardo@hotmail.com>
2022-02-19 21:31:58 -0300

    creating truncate for redis database

--- src/helpers/truncate.ts
+++ src/helpers/truncate.ts
@@ -1,8 +1,9 @@
 import { getConnection } from 'typeorm'
 
-const truncateDatabase = async (): Promise<void> => {
-  const connection = getConnection()
+import RedisConnection from '../config/redis/connection'
 
+const truncatePostgres = async () => {
+  const connection = getConnection()
   const entities = connection.entityMetadatas
 
   for (const entity of entities) {
@@ -10,6 +11,19 @@ const truncateDatabase = async (): Promise<void> => {
 
     await repository.clear()
   }
+}
+
+const truncateRedisKeys = async () => {
+  const connection = new RedisConnection()
+
+  const client = await connection.start()
+
+  client.flushAll()
+}
+
+const truncateDatabase = async (): Promise<void> => {
+  truncatePostgres()
+  truncateRedisKeys()
 }
 
 export default truncateDatabase
