92d735174b292aa37b6a251a69b60f842946c06e
eduahcb <dev_eduardo@hotmail.com>
2022-02-20 16:30:26 -0300

    changing methods to errorHandler

--- src/handlers/errorHandler.ts
+++ src/handlers/errorHandler.ts
@@ -1,11 +1,68 @@
+import { Request, Response, NextFunction } from 'express'
+import Youch from 'youch'
+
+import { config, rollbar } from '../config'
+
 class ErrorHandler {
-  handleError = async (error: any): Promise<boolean> => {
-    return this.isOperationalError(error)
+  private error: any
+  private req: Request
+  private res: Response
+  private nextFunction: NextFunction
+  private env: string
+
+  constructor(
+    error: any,
+    req: Request,
+    response: Response,
+    nextFunction: NextFunction
+  ) {
+    this.error = error
+    this.req = req
+    this.res = response
+    this.nextFunction = nextFunction
+    this.env = config.get('env')
   }
 
-  isOperationalError = (error: any) => {
-    return error.isOperational
+  private isOperationalError = () => {
+    return this.error.isOperational
+  }
+
+  private developmentResponse = async () => {
+    const youch = new Youch(this.error, this.req)
+    const result = await youch.toJSON()
+
+    this.res.status(500).json(result)
+  }
+
+  private responseErrors = async () => {
+    if (!this.isOperationalError()) {
+      if (this.env === 'development') {
+        return this.developmentResponse()
+      }
+
+      return this.res.status(500).json({
+        message: 'Internal server error',
+        statusCode: 500,
+      })
+    }
+
+    return this.res.status(this.error.statusCode).json({
+      message: this.error.message,
+      statusCode: this.error.statusCode,
+    })
+  }
+
+  private triggerErrorToRollbar = () => {
+    if (this.env === 'production' && !this.isOperationalError()) {
+      console.log('foi')
+      rollbar.error(this.error, this.req)
+    }
+  }
+
+  handle = async (): Promise<void> => {
+    this.triggerErrorToRollbar()
+    this.responseErrors()
   }
 }
 
-export default new ErrorHandler()
+export default ErrorHandler
--- src/middlewares/exceptionHandler.ts
+++ src/middlewares/exceptionHandler.ts
@@ -1,35 +1,14 @@
 import { NextFunction, Request, Response } from 'express'
-import Youch from 'youch'
 
-import { config } from '../config'
-import errorHandler from '../handlers'
+import ErrorHandler from '../handlers'
 
 export default async function exceptionHandler(
   error: any,
   req: Request,
   res: Response,
   next: NextFunction
-): Promise<Response> {
-  const isOperational = await errorHandler.handleError(error)
+): Promise<void> {
+  const errorHandler = new ErrorHandler(error, req, res, next)
 
-  if (!isOperational) {
-    const env = config.get('env')
-
-    if (env === 'development') {
-      const youch = new Youch(error, req)
-      const result = await youch.toJSON()
-
-      return res.status(500).json(result)
-    }
-
-    return res.status(500).json({
-      message: 'Internal server error',
-      statusCode: 500,
-    })
-  }
-
-  return res.status(error.statusCode).json({
-    message: error.message,
-    statusCode: error.statusCode,
-  })
+  await errorHandler.handle()
 }
