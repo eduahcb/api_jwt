4f4349a2b9428297e4ce3aed7e331db6b8af4ace
eduahcb <dev_eduardo@hotmail.com>
2022-02-25 21:47:15 -0300

    closing connection

--- src/config/redis/blockList.ts
+++ src/config/redis/blockList.ts
@@ -1,41 +1,37 @@
 import jwt from 'jsonwebtoken'
 import { createHash } from 'crypto'
 
-import RedisConnection from './connection'
+import redisClient from './connection'
 
 type JWT = {
   exp: number
 }
 
 class BlockList {
-  private connection: RedisConnection
-
-  constructor() {
-    this.connection = new RedisConnection()
-  }
-
   private generateTokenHash = async (token: string) => {
     return createHash('sha256').update(token).digest('hex')
   }
 
   add = async (token: string): Promise<void> => {
-    const client = await this.connection.start()
+    await redisClient.connect()
 
     const tokenHash = await this.generateTokenHash(token)
-
     const payload = jwt.decode(token) as JWT
 
-    await client.set(tokenHash, '', {
+    await redisClient.set(tokenHash, '', {
       EXAT: payload.exp,
     })
+
+    await redisClient.quit()
   }
 
   containsTokenHash = async (token: string): Promise<boolean> => {
-    const client = await this.connection.start()
+    await redisClient.connect()
 
     const tokenHash = await this.generateTokenHash(token)
+    const result = await redisClient.exists(tokenHash)
 
-    const result = await client.exists(tokenHash)
+    await redisClient.quit()
 
     return result === 1
   }
